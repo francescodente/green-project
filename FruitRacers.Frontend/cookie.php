0794ff2d1a917cf0878e30e05b6b38d7a85320a8
Gianni Tumedei <giannitumedei@gmail.com>
2019-10-24 17:59:32 +0200

    Create privacy.php

--- FruitRacers.Frontend/cookie.php
+++ FruitRacers.Frontend/cookie.php
@@ -1,7 +1,7 @@
 <!-- <div id="alert-cookie" class="alert alert-dismissible fade show" role="alert">
     <div class="alert-content">
         <div class="content">
-            Questo sito usa i cookie per garantire una migliore esperienza. <a href="#">Maggiori informazioni</a>
+            Questo sito usa i cookie per garantire una migliore esperienza. <a href="privacy.php">Maggiori informazioni</a>
         </div>
         <div class="actions">
             <button id="cookie-allow-button" type="button" class="btn transparent ripple" data-dismiss="alert" aria-label="Close">Ok</button>
