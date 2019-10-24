7094ed7d55d9ef1df02a0a09e7013badf60b52d7
Gianni Tumedei <giannitumedei@gmail.com>
2019-10-24 18:00:19 +0200

    Add sig-up modal to modal-login.php

--- FruitRacers.Frontend/modal-login.php
+++ FruitRacers.Frontend/modal-login.php
@@ -1,9 +1,10 @@
+<!-- LOGIN -->
 <div id="modal-login" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content d-flex flex-column">
             <div class="modal-top">
+                <i class="login-header-icon mdi mdi-account-circle"></i>
                 <button class="btn icon ripple" data-dismiss="modal" title="Chiudi"><i class="mdi dark mdi-close"></i></button>
-                <i id="login-header-icon" class="mdi mdi-account-circle"></i>
             </div>
             <form class="modal-body">
 
@@ -11,19 +12,21 @@
 
                 <!-- E-MAIL -->
                 <div class="text-input">
-                    <input id="email" type="email"/>
-                    <label for="email">Email</label>
+                    <input id="login-email" type="email" name="email"/>
+                    <label for="login-email">E-mail</label>
                 </div>
 
                 <!-- PASSWORD -->
                 <div class="text-input">
-                    <input id="password" type="password"/>
-                    <label for="password">Password</label>
+                    <input id="login-password" type="password" name="password"/>
+                    <label for="login-password">Password</label>
                 </div>
+
+                <span class="text-error-dark mb-2 d-none">E-mail o password errate</span>
 
                 <div class="d-flex justify-content-between align-items-center">
                     <!-- KEEP LOGIN -->
-                    <input id="keep-login" type="checkbox" class="checkbox" name="keep-login" value="1" />
+                    <input id="keep-login" type="checkbox" class="checkbox" name="keep-login" value="1"/>
                     <label for="keep-login" class="my-2">Ricordami</label><br>
 
                     <a href="#" class="text-sec-dark">Password dimenticata?</a>
@@ -31,9 +34,60 @@
 
                 <button id="submit-login" class="btn accent ripple my-3">Accedi</button>
 
-                <p class="text-center text-sec-dark m-0">Non hai un account? <a href="#">Registrati ora</a></p>
+                <p class="text-center text-sec-dark m-0">Non hai un account? <a id="sign-up-link" href="#">Registrati ora</a></p>
 
             </form>
         </div>
     </div>
 </div>
+
+<!-- SIGN-UP -->
+<div id="modal-sign-up" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content d-flex flex-column">
+            <div class="modal-top">
+                <i class="login-header-icon mdi mdi-account-circle"></i>
+                <button id="back-to-login" class="btn icon ripple" title="Indietro"><i class="mdi dark mdi-arrow-left"></i></button>
+                <button class="btn icon ripple" data-dismiss="modal" title="Chiudi"><i class="mdi dark mdi-close"></i></button>
+            </div>
+            <form class="modal-body">
+
+                <h4 class="text-center my-3">Registrazione</h4>
+
+                <!-- E-MAIL -->
+                <div class="text-input">
+                    <input id="sign-up-email" type="email" name="email"/>
+                    <label for="sign-up-email">E-mail</label>
+                    <span class="email-error d-none">Error message</span>
+                </div>
+
+                <!-- PASSWORD -->
+                <div class="text-input">
+                    <input id="sign-up-password" type="password" name="password"/>
+                    <label for="sign-up-password">Password</label>
+                    <span class="password-error d-none">Error message</span>
+                </div>
+
+                <!-- CONFIRM PASSWORD -->
+                <div class="text-input">
+                    <input id="confirm-password" type="password" name="confirm-password"/>
+                    <label for="confirm-password">Conferma password</label>
+                    <span class="confirm-password-error d-none">Error message</span>
+                </div>
+
+                <!-- PRIVACY CONSENT -->
+                <input id="privacy-consent" type="checkbox" class="checkbox" name="privacy-consent" value="1"/>
+                <label for="privacy-consent" class="my-2">Ho letto l'<a href="privacy.php" target="_blank">informativa sulla privacy</a></label><br>
+
+                <!-- MARKETING CONSENT -->
+                <input id="marketing-consent" type="checkbox" class="checkbox" name="marketing-consent" value="1"/>
+                <label for="marketing-consent" class="my-2">Vorrei ricevere informazioni di marketing</label><br>
+
+                <button id="submit-sign-up" class="btn accent ripple mt-3">Registrati</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="js/modal-login.js"></script>
