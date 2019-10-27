2a4eeb0e08ffc5fe20ec4ecfaf80f62dacb907ff
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-27 17:37:51 +0100

    Remove units table from sql project

--- FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/OrderDetails.sql
+++ FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/OrderDetails.sql
@@ -7,7 +7,6 @@
     [Price] MONEY NULL, 
     [UnitMultiplier] DECIMAL(8, 4) NULL, 
     CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY ([OrderID]) REFERENCES [Orders]([OrderID]),
-    CONSTRAINT [FK_OrderDetails_MeasurementUnits] FOREIGN KEY ([UnitName]) REFERENCES [MeasurementUnits]([UnitName]),
     CONSTRAINT [FK_OrderDetails_Products] FOREIGN KEY ([ProductID]) REFERENCES [Products]([ProductID]), 
     CONSTRAINT [PK_OrderDetails] PRIMARY KEY ([OrderID], [ProductID])
 )
