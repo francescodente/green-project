6324c8d7052908eab79fbba6959623a7838b06fd
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-27 17:41:52 +0100

    Create unit of work objects

--- FruitRacers.Backend/FruitRacers.Backend.Core/IDataSession.cs
+++ FruitRacers.Backend/FruitRacers.Backend.Core/IDataSession.cs
@@ -1,4 +1,5 @@
-﻿using FruitRacers.Backend.Core.Repositories;
+﻿using FruitRacers.Backend.Core.Entities;
+using FruitRacers.Backend.Core.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,11 @@ namespace FruitRacers.Backend.Core
 {
     public interface IDataSession : IDisposable
     {
-        
+        IUserRepository Users { get; }
+        IOrderRepository Orders { get; }
+        IProductRepository Products { get; }
+        ITimeSlotRepository TimeSlots { get; }
+        IReadOnlyRepository<Category> Categories { get; }
+        IRepository<Address> Addresses { get; }
     }
 }
