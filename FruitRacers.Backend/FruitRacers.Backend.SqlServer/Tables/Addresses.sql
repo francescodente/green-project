c0054aabbc00c7ff6e49fdc830243126ec5d220d
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-29 14:03:40 +0100

    Change lat/long type to float

--- FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/Addresses.sql
+++ FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/Addresses.sql
@@ -2,8 +2,8 @@
 (
 	[AddressID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Description] NVARCHAR(100) NOT NULL, 
-    [Latitude] REAL NOT NULL, 
-    [Longitude] REAL NOT NULL, 
+    [Latitude] FLOAT NOT NULL, 
+    [Longitude] FLOAT NOT NULL, 
     [UserID] INT NOT NULL, 
     CONSTRAINT [FK_Addresses_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([UserID])
 )
