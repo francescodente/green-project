96dccd1ffa0d368f661d0ee2aa8cf46f8ac7d381
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-29 14:04:24 +0100

    Remove order states table

--- FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/OrderStates.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[OrderStates]
-(
-	[OrderStateID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [StateName] NVARCHAR(30) NOT NULL
-)
--- FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/Orders.sql
+++ FruitRacers.Backend/FruitRacers.Backend.SqlServer/Tables/Orders.sql
@@ -4,11 +4,10 @@
     [Notes] NVARCHAR(1000) NULL, 
     [DeliveryDate] DATE NULL, 
     [Timestamp] TIMESTAMP NULL, 
-    [OrderStateID] INT NOT NULL, 
+    [OrderState] INT NOT NULL, 
     [TimeSlotID] INT NULL, 
     [UserID] INT NOT NULL, 
     [AddressID] INT NULL, 
-    CONSTRAINT [FK_Orders_OrderStates] FOREIGN KEY ([OrderStateID]) REFERENCES [OrderStates]([OrderStateID]),
     CONSTRAINT [FK_Orders_TimeSlots] FOREIGN KEY ([TimeSlotID]) REFERENCES [TimeSlots]([TimeSlotID]), 
 	CONSTRAINT [FK_Orders_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([UserID]), 
 	CONSTRAINT [FK_Orders_Addresses] FOREIGN KEY ([AddressID]) REFERENCES [Addresses]([AddressID])
