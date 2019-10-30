abc57b347b6c95dd3feb7a80a55bcef76c4fe2a6
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-30 09:52:06 +0100

    Move Unit tests in a separate folder

--- FruitRacers.Backend/Fruitracers.Backend.Test/UnitTests/Services/TestCategoriesService.cs
+++ FruitRacers.Backend/Fruitracers.Backend.Test/UnitTests/Services/TestCategoriesService.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fruitracers.Backend.Test
+namespace Fruitracers.Backend.Test.UnitTests.Services
 {
     public class TestCategoriesService
     {
