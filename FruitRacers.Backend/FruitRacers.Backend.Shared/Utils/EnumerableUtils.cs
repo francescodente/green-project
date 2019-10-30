482ff7774b9397348b41bbb278397fb25f2880a3
Francesco Dente <dente.francesco1996@gmail.com>
2019-10-30 09:32:18 +0100

    Create first utility classes

---
+++ FruitRacers.Backend/FruitRacers.Backend.Shared/Utils/EnumerableUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FruitRacers.Backend.Shared.Utils
+{
+    public static class EnumerableUtils
+    {
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T item in sequence)
+            {
+                action(item);
+            }
+        }
+    }
+}
