1eb4e9cc345516a9a8ae1b3e0549c79eead8f4e3
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-09 17:12:38 +0100

    Create mapping utils

---
+++ FruitRacers.Backend/FruitRacers.Backend.ApiLayer/DependencyInjection/AutoMapperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FruitRacers.Backend.Core.Services.Utils;
+
+namespace FruitRacers.Backend.ApiLayer.DependencyInjection
+{
+    public static class AutoMapperExtensions
+    {
+        public static IServiceCollection AddDtoMappers(this IServiceCollection services)
+        {
+            return services.AddSingleton(MappingUtils.CreateDefaultMapper());
+        }
+    }
+}
---
+++ FruitRacers.Backend/FruitRacers.Backend.Core/Services/Utils/MappingUtils.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FruitRacers.Backend.Core.Services.Utils
+{
+    public static class MappingUtils
+    {
+        public static IMapper CreateDefaultMapper()
+        {
+            MapperConfiguration config = new MapperConfiguration(c =>
+            {
+
+            });
+            return config.CreateMapper();
+        }
+    }
+}
