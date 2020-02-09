842bc4117d152242eb036962161c4af184ad0b0a
Gianni Tumedei <giannitumedei@gmail.com>
2020-02-09 14:54:42 +0100

    Resedign footer (hopefully for the last time)

--- FruitRacers.Frontend/footer.php
+++ FruitRacers.Frontend/footer.php
@@ -1,44 +1,80 @@
 <footer class="footer">
-    <div class="container text-center mb-4">
+    <div class="container my-4">
         <div class="row">
-            <div class="col-12 mt-4 mb-3">
+
+            <!-- Back to top button -->
+            <div class="col-12 text-center mb-4 order-0">
                 <button id="go-top-btn" class="btn round outline ripple text-center">
                     <i class="mdi dark mdi-chevron-up"></i>
                     <span class="text-sec-dark">Torna su</span>
                 </button>
             </div>
-            <div class="col-12 mt-3 mb-4">
-                <a href="index.php" class="footer-logo d-block"><img src="images/logo/fruitracers_logo_small.png"></a>
+
+            <!-- Logo -->
+            <div class="col-6 col-lg-4 order-1 order-lg-4">
+                <a href="index.php" class="footer-logo d-inline-block"><img src="images/logo/fruitracers_logo_small.png"></a>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-4 mb-3 mb-md-0 order-md-2">
-                <h6 class="h-variant text-accent">Mappa del sito</h6>
+
+            <!-- Social -->
+            <div class="col-6 col-lg-4 d-flex justify-content-end align-items-end order-2 order-lg-6">
+                <div>
+                    <a href="https://www.facebook.com" target="_blank" class="facebook btn icon dark ripple" title="Facebook">
+                        <i class="mdi mdi-facebook-box"></i>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" class="instagram btn icon dark ripple" title="Instagram">
+                        <i class="mdi mdi-instagram"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-12 d-lg-none order-3">
+                <div class="divider dark my-4"></div>
+            </div>
+
+            <!-- Site map -->
+            <div class="col-12 col-lg-4 text-center text-lg-left order-4 order-lg-1">
+                <h6 class="h-variant text-accent">Sito</h6>
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="index.php#about">Chi siamo</a></li>
                     <li><a href="index.php#contacts">Contatti</a></li>
                     <li><a href="companies.php">Partner</a></li>
+                    <li><a href="faq.php">Aiuto</a></li>
+                    <li><a href="privacy-terms.php">Privacy e termini</a></li>
                 </ul>
             </div>
-            <div class="col-12 col-md-4 mb-3 mb-md-0 order-md-3">
-                <h6 class="h-variant text-accent">Social</h6>
-                <div class="mb-3">
-                    <a href="https://www.facebook.com" target="_blank" class="btn icon dark ripple" title="Facebook"><i class="mdi dark mdi-facebook-box"></i></a>
-                    <a href="https://www.instagram.com" target="_blank" class="btn icon dark ripple" title="Instagram"><i class="mdi dark mdi-instagram"></i></a>
-                </div>
+
+            <!-- Info & contacts -->
+            <div class="col-12 col-lg-4 text-center mt-4 mt-lg-0 order-5 order-lg-2">
+                <h6 class="h-variant text-accent mb-3">Contatti</h6>
+                <i class="mdi dark mdi-email"></i>
+                <p>
+                    <a href="mailto:info@fruitracers.com" target="_top">info@fruitracers.com</a><br>
+                    <a href="mailto:support@fruitracers.com" target="_top">support@fruitracers.com</a>
+                </p>
+                <i class="mdi dark mdi-phone"></i>
+                <p class="m-0">+39 123 456 7890</p>
+            </div>
+
+            <!-- Timetable -->
+            <div class="col-12 col-lg-4 text-center text-lg-right mt-4 mt-lg-0 order-6 order-lg-3">
                 <h6 class="h-variant text-accent">Orari di consegna</h6>
-                <ul class="timetable">
-                    <li><span>LUN - VEN</span><span>8-13, 15-18</span></li>
-                    <li><span>SAB, DOM</span><span>-</span></li>
-                </ul>
+                <table class="text-sec-dark ml-auto mr-auto mr-lg-0">
+                    <tr><td>Lunedì</td><td>8-13, 15-18</td></tr>
+                    <tr><td>Martedì</td><td>8-13, 15-18</td></tr>
+                    <tr><td>Mercoledì</td><td>8-13, 15-18</td></tr>
+                    <tr><td>Giovedì</td><td>8-13, 15-18</td></tr>
+                    <tr><td>Venerdì</td><td>8-13, 15-18</td></tr>
+                    <tr><td>Sabato</td><td>-</td></tr>
+                    <tr><td>Domenica</td><td>-</td></tr>
+                </table>
             </div>
-            <div class="col-12 col-md-4 order-md-1">
-                <h6 class="h-variant text-accent">Privacy</h6>
-                <a href="privacy-terms.php" class="d-block mb-3">Informativa</a>
-                <h6 class="h-variant text-accent">Copyright</h6>
-                <p class="m-0">&copy; <?php echo date("Y"); ?> Fruitracers™</p>
+
+            <!-- Copyright -->
+            <div class="col-12 col-lg-4 d-flex justify-content-center align-items-end mt-4 mt-lg-0 order-7 order-lg-5">
+                <p class="m-0">&copy; Copyright <?php echo date("Y"); ?> Fruitracers™</p>
             </div>
+
         </div>
     </div>
 </footer>
