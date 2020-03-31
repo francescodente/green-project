072fbe0e0433f18c75f2b5b4a635cbcce53a077d
Gianni <giannitumedei@gmail.com>
2020-03-31 12:45:54 +0200

    Polish cookie banner and show it on the website homepage

--- GreenProject.Frontend/cookie.php
+++ GreenProject.Frontend/cookie.php
@@ -1,10 +1,10 @@
-<!-- <div id="alert-cookie" class="alert alert-dismissible fade show" role="alert">
-    <div class="alert-content">
+<div id="alert-cookie" class="alert alert-dismissible fade show" role="alert">
+    <div class="alert-content d-flex align-items-center">
         <div class="content">
             Questo sito usa i cookie per garantire una migliore esperienza. <a href="privacy-terms.php">Maggiori informazioni</a>
         </div>
-        <div class="actions">
+        <div class="actions d-flex">
             <button id="cookie-allow-button" type="button" class="btn transparent ripple" data-dismiss="alert" aria-label="Close">Ok</button>
         </div>
     </div>
-</div> -->
+</div>
