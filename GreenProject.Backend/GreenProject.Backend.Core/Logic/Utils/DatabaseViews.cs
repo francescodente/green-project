bcdd4272f6ee0dbcf86dc7ec2ee91b50d1a91c3b
Francesco Dente <dente.francesco1996@gmail.com>
2020-05-06 01:40:30 +0200

    Add common database views

---
+++ GreenProject.Backend/GreenProject.Backend.Core/Logic/Utils/DatabaseViews.cs
@@ -0,0 +1,52 @@
+﻿using GreenProject.Backend.Core.Utils.Session;
+using GreenProject.Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenProject.Backend.Core.Logic.Utils
+{
+    public static class DatabaseViews
+    {
+        public static IQueryable<User> ActiveUsers(this IDataSession data)
+        {
+            return data.Users.Where(u => !u.IsDeleted);
+        }
+
+        public static IQueryable<User> EnabledUsers(this IDataSession data)
+        {
+            return data.ActiveUsers().Where(u => u.IsEnabled);
+        }
+
+        public static IQueryable<PurchasableItem> ActivePurchasableItems(this IDataSession data)
+        {
+            return data.PurchasableItems.Where(p => !p.IsDeleted);
+        }
+
+        public static IQueryable<PurchasableItem> VisiblePurchasableItems(this IDataSession data)
+        {
+            return data.ActivePurchasableItems().Where(p => p.IsEnabled);
+        }
+
+        public static IQueryable<Product> ActiveProducts(this IDataSession data)
+        {
+            return data.Products.Where(p => !p.IsDeleted);
+        }
+
+        public static IQueryable<Product> VisibleProducts(this IDataSession data)
+        {
+            return data.ActiveProducts().Where(p => p.IsEnabled);
+        }
+
+        public static IQueryable<Crate> ActiveCrates(this IDataSession data)
+        {
+            return data.Crates.Where(c => !c.IsDeleted);
+        }
+
+        public static IQueryable<Crate> VisibleCrates(this IDataSession data)
+        {
+            return data.ActiveCrates().Where(c => c.IsEnabled);
+        }
+    }
+}
